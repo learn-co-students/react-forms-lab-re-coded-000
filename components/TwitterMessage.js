1ede7578049a78e481bcc52bc17d9d858bcdcc4d
ALI ALRAWI <ali.developer90@gmail.com>
2017-02-25 20:59:06 +0000

    Done.

--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -3,18 +3,37 @@ const React = require('react');
 class TwitterMessage extends React.Component {
   constructor() {
     super();
+    this.trackChars=function(){return this.props.maxChars - this.state.value.length}
 
-    this.state = {};
+    this.callBack=this.callBack.bind(this)
+
+    this.state = {
+      maxChars:10,
+      value:""
+     };
+
   }
+  callBack(e){
+    this.setState({
+      value: e.target.value,
+    });
+            }
 
   render() {
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" />
+        <strong >Your message:</strong>
+        <input onChange={this.callBack} value={this.state.value} type="text" />
+        <small >{this.trackChars()}></small>
       </div>
     );
   }
 }
+TwitterMessage.defultProps={
+  maxChars: 100
+}
+TwitterMessage.PropTypes ={
+  maxChars: React.PropTypes.number,
 
+}
 module.exports = TwitterMessage;
